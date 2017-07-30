1bb628e4afb113b7006a738bbf12edd0d4c482f2
technetos <mrgould93@gmail.com>
2017-07-30 19:13:57 -0400

    added basic structure for nodes

--- hatch/src/lib.rs
+++ hatch/src/lib.rs
@@ -1,6 +1,49 @@
+mod node
+{
+  pub struct file
+  {
+    file_name: String,
+  }
+
+  pub struct dir
+  {
+    dir_name: String,
+  }
+
+  impl file
+  {
+    pub fn new (file_name: &str) -> file
+    {
+      file { file_name: String::from (file_name) }
+    }
+  }
+
+  impl dir
+  {
+    pub fn new (dir_name: &str) -> dir
+    {
+      dir { dir_name: String::from (dir_name) }
+    }
+  }
+
+  pub enum node_type
+  {
+    leaf (file),
+    node (dir),
+  }
+}
+
+
 #[cfg(test)]
 mod tests {
+
+  use super::node::node_type;
+  use super::node::file;
+  use super::node::dir;
+
     #[test]
     fn it_works() {
+      node_type::leaf (file::new ("hatch"));
+      node_type::node (dir::new ("hatch"));
     }
 }
