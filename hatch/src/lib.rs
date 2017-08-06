066cbf0fbbb77b42ecdf7071c47d5045ed4c43b5
technetos <mrgould93@gmail.com>
2017-08-06 16:30:09 -0400

    replaced hashmap with vector, added methods to dir and file, setup data module for file content temmplates

--- hatch/src/lib.rs
+++ hatch/src/lib.rs
@@ -1,49 +1,66 @@
-mod node
-{
-  pub struct file
-  {
-    file_name: String,
-  }
-
-  pub struct dir
-  {
-    dir_name: String,
-  }
-
-  impl file
-  {
-    pub fn new (file_name: &str) -> file
-    {
-      file { file_name: String::from (file_name) }
-    }
-  }
-
-  impl dir
-  {
-    pub fn new (dir_name: &str) -> dir
-    {
-      dir { dir_name: String::from (dir_name) }
-    }
-  }
-
-  pub enum node_type
-  {
-    leaf (file),
-    node (dir),
+mod data {
+  pub enum Template {
+    tupfile,
+    makefile,
   }
 }
 
+mod node {
+  pub struct file {
+    contents: String,
+    file_name: String,
+  }
+
+  pub struct dir {
+    contents: Vec <NodeType>,
+    dir_name: String,
+  }
+
+  impl file {
+    pub fn new(file_name: &str) -> file {
+      file {
+        contents: "".to_string(),
+        file_name: file_name.to_string()
+      }
+    }
+
+    pub fn update_content(&mut self, data: super::data::Template) {
+    }
+  }
+
+  impl dir {
+    pub fn new(dir_name: &str) -> dir {
+      dir {
+        contents: Vec::new(),
+        dir_name: dir_name.to_string(),
+      }
+    }
+    
+    pub fn add_file(&mut self, key: &str) {
+      self.contents.push(NodeType::leaf(file::new(key)));
+    }
+
+    pub fn add_dir(&mut self, key: &str) {
+      self.contents.push(NodeType::node(dir::new(key)));
+    }
+  }
+
+  pub enum NodeType {
+    leaf(file),
+    node(dir),
+  }
+}
 
 #[cfg(test)]
 mod tests {
-
-  use super::node::node_type;
+  use super::node::NodeType;
   use super::node::file;
   use super::node::dir;
 
     #[test]
     fn it_works() {
-      node_type::leaf (file::new ("hatch"));
-      node_type::node (dir::new ("hatch"));
+      let mut root = dir::new("project_root");
+
+      root.add_file("README.md");
     }
 }
