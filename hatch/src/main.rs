de283431b9e6401d9890b2bf021458f715aed64b
technetos <mrgould93@gmail.com>
2017-09-16 15:11:21 -0400

    Decided that I way overshot the engineering on this and simplified it

--- hatch/src/main.rs
+++ hatch/src/main.rs
@@ -1,23 +1,44 @@
 #[macro_use]
 extern crate clap;
+use clap::ArgMatches;
 
 mod cli;
 mod project;
 
 use project::Project;
-use project::ProjectType;
-use project::LibraryType;
+use project::ProjectType::{ Binary, Library };
+use project::LibraryType::{ Shared, Static };
 
 fn main() {
-  match cli::build_cli().get_matches().subcommand() {
-    ("new", Some(args)) => create_new_project(Project::new_project(args)),
-    _ => println!("under construction"),
+  let project = match cli::build_cli()
+    .get_matches()
+    .subcommand() {
+
+      ("new", Some(args)) => create_new_project(args),
+      _ => help(),
+    };
+
+
+  match project {
+    Ok(p) => println!("{:?}", p),
+    Err(e) => println!("{}", e),
   }
 }
 
-fn create_new_project(project: Project) {
-  match project.project_type {
-    ProjectType::Binary => println!("binary"),
-    ProjectType::Library(lib_type) => println!("{:?}", lib_type),
-  }
+fn help<'a>() -> Result<Project, &'a str> {
+  Err("No arguments were given")
+}
+
+fn create_new_project<'a>(args: &ArgMatches) -> Result<Project, &'a str> {
+  let project_name = value_t!(args, "PROJECT_NAME", String).unwrap();
+  let project_type =  
+    if args.is_present("bin") { Binary }
+    else {
+      match args.is_present("static") {
+        true => Library(Static),
+        false => Library(Shared),
+      }
+    };
+
+  Ok(Project { project_name, project_type })
 }
--- hatch/src/project/mod.rs
+++ hatch/src/project/mod.rs
@@ -1,8 +1,13 @@
-extern crate clap;
-
+#[derive(Debug)]
 pub struct Project {
   pub project_name: String,
   pub project_type: ProjectType,
+}
+
+#[derive(Debug)]
+pub enum ProjectType {
+  Binary,
+  Library(LibraryType),
 }
 
 #[derive(Debug)]
@@ -10,34 +15,3 @@ pub enum LibraryType {
   Shared,
   Static,
 }
-
-pub enum ProjectType {
-  Binary,
-  Library(LibraryType),
-}
-
-impl Project {
-  pub fn new_project(args: &clap::ArgMatches) -> Project {
-    let project_name = value_t!(args, "PROJECT_NAME", String).unwrap();
-
-    let project_type = if args.is_present("bin") {
-      ProjectType::Binary
-    } else {
-      if args.is_present("static") {
-        ProjectType::Library(LibraryType::Static)
-      } else {
-        ProjectType::Library(LibraryType::Shared)
-      }
-    };
-
-    Project {
-      project_name,
-      project_type,
-    }
-  }
-}
-
-// TODO:
-// Implement trait that ProjectType & LibraryType conform to
-// such that we can act upon any kind of project be it a
-// binary or a library shared or static
