c130861f28f1585b77bab81d2891194b96f9dcc1
technetos <mrgould93@gmail.com>
2017-09-18 17:28:04 -0400

    Added default setting through clap::AppSettings to the cli module to have a default help displayed when no subcommands are given and removed the help fn from main.rs and the hack that went along w/ it

--- hatch/src/main.rs
+++ hatch/src/main.rs
@@ -18,8 +18,7 @@ fn main() {
 
       ("new", Some(args)) => create_new_project(args),
       ("update", Some(args)) => update_existing_project(args),
-
-      _ => help(),
+      _ => Err("")
     };
 
 
@@ -27,10 +26,6 @@ fn main() {
     Ok(project) => println!("{:?}", project),
     Err(e) => println!("{}", e),
   }
-}
-
-fn help<'a>() -> Result<Project, &'a str> {
-  Err("No arguments were given")
 }
 
 fn create_new_project<'a>(args: &ArgMatches) -> Result<Project, &'a str> {
