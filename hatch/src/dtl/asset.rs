3e545ef0c40760a54665960797c3ea418b30925e
technetos <mrgould93@gmail.com>
2017-10-16 12:14:31 -0400

    implemented input_file, read, write and output_file methods onto dtl/assets

--- hatch/src/dtl/asset.rs
+++ hatch/src/dtl/asset.rs
@@ -2,7 +2,7 @@ pub struct AssetOptions {
   input_file: String,
   read: bool,
   write: bool,
-  output_file: String,
+  output_file: Option<String>,
 }
 
 impl AssetOptions {
@@ -11,7 +11,25 @@ impl AssetOptions {
       input_file: String::new(),
       read: false,
       write: false,
-      output_file: String::new(),
+      output_file: None,
+    }
+  }
+
+  pub fn input_file(&mut self, file_name: &str) {
+    self.input_file.push_str(file_name);
+  }
+
+  pub fn read(&mut self, read: bool) {
+    self.read = read;
+  }
+
+  pub fn write(&mut self, write: bool) {
+    self.write = write;
+  }
+
+  pub fn output_file(&mut self, file_name: Option<&str>) {
+    if let Some(s) = file_name {
+      self.output_file = Some(String::from(s));
     }
   }
 }
