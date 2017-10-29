cba07eddcd8b29f5cb27d1474e12fd30d419d5cb
technetos <mrgould93@gmail.com>
2017-10-28 23:41:18 -0400

    implemented fluent builder for assets

--- hatch/src/asset.rs
+++ hatch/src/asset.rs
@@ -9,7 +9,35 @@ impl Asset {
     Asset(asset_imp::AssetBuilder::new())
   }
 
-  pub fn build_tuprules(&mut self) {
-    self.0.build_tuprules();
+  pub fn build_tuprules(&mut self) -> &mut Asset {
+    self.0.build_tuprules(); self
+  }
+
+  pub fn build_tupfile(&mut self) -> &mut Asset {
+    self.0.build_tupfile(); self
+  }
+
+  pub fn build_project_config(&mut self) -> &mut Asset {
+    self.0.build_project_config(); self
+  }
+
+  pub fn build_project_tupfile(&mut self) -> &mut Asset {
+    self.0.build_project_tupfile(); self
+  }
+
+  pub fn build_project_test_tupfile(&mut self) -> &mut Asset {
+    self.0.build_project_test_tupfile(); self
+  }
+
+  pub fn build_linux_platform(&mut self) -> &mut Asset {
+    self.0.build_linux_platform(); self
+  }
+
+  pub fn build_darwin_platform(&mut self) -> &mut Asset {
+    self.0.build_darwin_platform(); self
+  }
+
+  pub fn build_win32_platform(&mut self) -> &mut Asset {
+    self.0.build_win32_platform(); self
   }
 }
