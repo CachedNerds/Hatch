d3e807873f805da2793e10123f5557f35a1c5295
technetos <mrgould93@gmail.com>
2017-12-21 18:23:55 -0500

    cleaned up some

--- hatch/src/hatch_error/mod.rs
+++ hatch/src/hatch_error/mod.rs
@@ -1,29 +1,31 @@
+use std::fmt;
 use std::error::Error;
+
 use std::io;
 use yaml_rust::scanner;
-use std::fmt;
 
+pub mod null;
+pub mod empty_config;
 pub mod missing_build;
 pub mod missing_name;
 pub mod missing_version;
-pub mod null;
-pub mod empty_config;
 
+
+pub use self::null::NullError;
+pub use self::empty_config::EmptyConfigError;
 pub use self::missing_name::MissingNameError;
 pub use self::missing_build::MissingBuildError;
 pub use self::missing_version::MissingVersionError;
-pub use self::null::NullError;
-pub use self::empty_config::EmptyConfigError;
 
 #[derive(Debug)]
 pub enum HatchError {
   Io(io::Error),
   Parsing(scanner::ScanError),
   Null(NullError),
+  EmptyConfig(EmptyConfigError),
   MissingName(MissingNameError),
   MissingBuild(MissingBuildError),
   MissingVersion(MissingVersionError),
-  EmptyConfig(EmptyConfigError)
 }
 
 impl Error for HatchError {
@@ -32,10 +34,10 @@ impl Error for HatchError {
       HatchError::Io(ref e) => e.description(),
       HatchError::Parsing(ref e) => e.description(),
       HatchError::Null(ref e) => e.description(),
+      HatchError::EmptyConfig(ref e) => e.description(),
       HatchError::MissingName(ref e) => e.description(),
       HatchError::MissingBuild(ref e) => e.description(),
       HatchError::MissingVersion(ref e) => e.description(),
-      HatchError::EmptyConfig(ref e) => e.description()
     }
   }
 
@@ -44,10 +46,10 @@ impl Error for HatchError {
       HatchError::Io(ref e) => e.cause(),
       HatchError::Parsing(ref e) => e.cause(),
       HatchError::Null(ref e) => e.cause(),
+      HatchError::EmptyConfig(ref e) => e.cause(),
       HatchError::MissingName(ref e) => e.cause(),
       HatchError::MissingBuild(ref e) => e.cause(),
       HatchError::MissingVersion(ref e) => e.cause(),
-      HatchError::EmptyConfig(ref e) => e.cause(),
     }
   }
 }
@@ -58,10 +60,10 @@ impl fmt::Display for HatchError {
       HatchError::Io(ref e) => write!(f, "IO error: {}", e),
       HatchError::Parsing(ref e) => write!(f, "Parsing error: {}", e),
       HatchError::Null(ref e) => write!(f, "{}", e),
+      HatchError::EmptyConfig(ref e) => write!(f, "Config error: {}", e),
       HatchError::MissingName(ref e) => write!(f, "Config error: {}", e),
       HatchError::MissingBuild(ref e) => write!(f, "Config error: {}", e),
       HatchError::MissingVersion(ref e) => write!(f, "Config error: {}", e),
-      HatchError::EmptyConfig(ref e) => write!(f, "Config error: {}", e)
     }
   }
 }
