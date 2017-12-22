dcf35067aecdfec9d6f323fe6ce7f10ae4ec45e6
technetos <mrgould93@gmail.com>
2017-12-22 17:23:58 -0500

    implemented Display for ProjectKind so we can define how it gets written to the file

--- hatch/src/project.rs
+++ hatch/src/project.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use dtl::project as project_imp;
 
 pub struct Project (project_imp::Project);
@@ -29,5 +31,15 @@ pub enum ProjectKind { Binary, Library(LibraryKind) }
 impl AsRef<ProjectKind> for ProjectKind {
   fn as_ref(&self) -> &ProjectKind {
     &self
+  }
+}
+
+impl fmt::Display for ProjectKind {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match *self {
+      ProjectKind::Library(LibraryKind::Shared) => write!(f, "shared-lib"),
+      ProjectKind::Library(LibraryKind::Static) => write!(f, "static-lib"),
+      ProjectKind::Binary => write!(f, "binary"),
+    }
   }
 }
