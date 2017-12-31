10aa087dfedb8cea8901035b5000433998f0e56a
technetos <mrgould93@gmail.com>
2017-12-31 16:16:05 -0500

    updated new to return a vector

--- hatch/src/cli/commands/new.rs
+++ hatch/src/cli/commands/new.rs
@@ -1,5 +1,5 @@
 use clap::{ App, SubCommand, Arg, ArgMatches };
-use cli::commands::Command;
+use cli::commands::{ Command };
 
 use project::{ Project, ProjectKind, LibraryKind };
 
@@ -15,12 +15,14 @@ use hatch_error::{
 };
 
 pub struct New {
-  name: &'static str
+  name: &'static str,
 }
 
 impl<'new> New {
   pub fn new() -> New {
-    New { name: "new" }
+    New {
+      name: "new",
+    }
   }
   
   fn project_version(&self, args: &ArgMatches<'new>) -> String {
@@ -47,34 +49,40 @@ impl<'command> Command<'command> for New {
     SubCommand::with_name(&self.name)
       .about("Creates a new project. (default = shared library)")
 
-      .arg(Arg::with_name("PROJECT_NAME")
+      .arg(Arg::with_name("PROJECT_NAMES")
            .help("Name of project")
-           .required(true).takes_value(true))
+           .takes_value(true).max_values(1)
+           .required(true))
 
       .arg(Arg::with_name("bin")
            .help("Generate a stand alone executable")
-           .long("bin").short("b").required(false)) 
+           .long("bin").short("b")
+           .required(false)) 
 
       .arg(Arg::with_name("static")
            .help("Generate a static library")
-           .long("static").short("s").conflicts_with("bin").required(false))
+           .long("static").short("s").conflicts_with("bin")
+           .required(false))
 
       .arg(Arg::with_name("VERSION")
            .help("Set the project version")
-           .long("version").short("v").required(false).takes_value(true))
+           .long("version").short("v").takes_value(true)
+           .required(false))
   }
 
   fn subcommand_name(&self) -> &'static str {
     self.name
   }
 
-  fn execute(&self, args: &ArgMatches<'command>) -> Result<Vec<Project>, HatchError> {
-    let name = self.project_name(args).unwrap();
+  fn execute(&self, args: &ArgMatches<'command>) -> Vec<Result<Project, HatchError>> {
+    let name = self.project_names(args).into_iter().nth(0).unwrap();
 
-    match fs::create_dir(self.project_path(args) + &name.as_str()) {
-      Err(e) => Err(HatchError::from(e)),
+    let dir_path = self.project_path(args) + &name[..];
+    let hatch_file = self.project_path(args) + &name[..] + "/Hatch.yml";
+
+    match fs::create_dir(&dir_path) {
+      Err(e) => vec![Err(HatchError::from(e))],
       Ok(_) => {
-        let path = self.project_path(args);
         let version = self.project_version(args);
         let kind = self.project_kind(args);
 
@@ -82,12 +90,12 @@ impl<'command> Command<'command> for New {
 
         write!(&mut yaml_output, "name: {}\nversion: {}\nbuild: {}\n", &name, &version, &kind);
 
-        match fs::File::create(self.project_path(args) + &name.as_str() + "/Hatch.yml") {
-          Err(e) => Err(HatchError::from(e)),
+        match fs::File::create(hatch_file) {
+          Err(e) => vec![Err(HatchError::from(e))],
           Ok(mut file) => {
             match file.write_all(yaml_output.as_bytes()) {
-              Err(e) => Err(HatchError::from(e)),
-              Ok(_) => Ok(vec![Project::new(name, kind, path, version)])
+              Err(e) => vec![Err(HatchError::from(e))],
+              Ok(_) => vec![Ok(Project::new(name, kind, version))]
             }
           }
         }
