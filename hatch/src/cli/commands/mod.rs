023827b402f3ddbcb2c5afd77d44f5d1e22f5ceb
technetos <mrgould93@gmail.com>
2017-12-31 16:28:14 -0500

    changed project_name() to project_names() to return a vector of project names instead of just one

--- hatch/src/cli/commands/mod.rs
+++ hatch/src/cli/commands/mod.rs
@@ -11,13 +11,13 @@ pub trait Command<'command> {
   
   fn subcommand_name(&self) -> &'static str;
 
-  fn execute(&self, args: &ArgMatches<'command>) -> Result<Vec<Project>, HatchError>;
-
-  fn project_name(&self, args: &ArgMatches<'command>) -> Option<String> {
-    if args.is_present("PROJECT_NAME") {
-      Some(value_t!(args, "PROJECT_NAME", String).unwrap())
+  fn execute(&self, args: &ArgMatches<'command>) -> Vec<Result<Project, HatchError>>;
+ 
+  fn project_names(&self, args: &ArgMatches<'command>) -> Vec<String> {
+    if args.is_present("PROJECT_NAMES") {
+      values_t!(args, "PROJECT_NAMES", String).unwrap()
     } else {
-      None
+      Vec::new()
     }
   }
   
