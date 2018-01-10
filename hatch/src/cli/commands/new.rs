acfc3e2f449caa61340da5d02f055df1a0b9c2bb
technetos <mrgould93@gmail.com>
2018-01-09 23:32:33 -0500

    clone dependencies for project on project creation

--- hatch/src/cli/commands/new.rs
+++ hatch/src/cli/commands/new.rs
@@ -1,13 +1,17 @@
 use HatchResult;
 use std::fs;
+use std::collections::HashMap;
 use clap::{ App, SubCommand, Arg, ArgMatches };
 use cli::commands::{ Command };
+use cli::commands::ops::RepoManager;
 use project::{ Project, ProjectKind, LibraryKind };
 use hatch_error::HatchError;
 
 // Must use qualified names to avoid conflict.
 use std::fmt::Write as FmtWrite;
 use std::io::Write as IoWrite;
+
+use git2;
 
 use cli::commands::{ INCLUDE, VERSION, STATIC, BIN, PROJECT_NAMES };
 
@@ -40,9 +44,30 @@ impl<'new> New {
     }
   }
 
-  fn get_includes(&self, args: &ArgMatches<'new>) -> Vec<String> {
+  fn create_modules_directory(&self, path: &str) {
+    let _ = fs::create_dir(self.modules_path(path));
+  }
+}
+
+impl<'new> RepoManager<'new> for New {
+  fn fetch_dependencies(&self, repo_spec: &Vec<(String, String)>, path: &str) {
+    repo_spec.iter().for_each(|repo| {
+      git2::Repository::clone(&repo.0, &repo.1);
+    });
+  }
+
+  fn get_includes(&self, args: &ArgMatches<'new>) -> Vec<(String, String)> {
+    let mut parsed_includes = Vec::new();
     match args.values_of(INCLUDE) {
-      Some(values) => values.map(String::from).collect(),
+      Some(values) => {
+        let mut vals = values.map(String::from).collect::<Vec<String>>().into_iter();
+        while vals.len() != 0 {
+          let mut left = vals.next();
+          let mut right = vals.next();
+          parsed_includes.push((left.unwrap(), right.unwrap()));
+        }
+        parsed_includes
+      },
       None => vec![],
     }
   }
@@ -75,7 +100,7 @@ impl<'command> Command<'command> for New {
 
       .arg(Arg::with_name(INCLUDE)
            .help("List URLs to git repositories")
-           .long("include").short("i").min_values(1).takes_value(true)
+           .long("include").short("i").multiple(true).number_of_values(2).takes_value(true)
            .required(false))
   }
 
@@ -94,7 +119,20 @@ impl<'command> Command<'command> for New {
       Ok(_) => {
         let version = self.project_version(args);
         let kind = self.project_kind(args);
-        let includes =  String::from("  - ") + &self.get_includes(args).join("\n  - ");
+
+        self.create_modules_directory(&dir_path);
+
+        if args.is_present(INCLUDE) {
+          self.fetch_dependencies(&self.get_includes(args).as_mut(), &self.modules_path(&dir_path));
+        }
+
+        let mut includes = String::new();
+
+        self.get_includes(args).iter().for_each(|&(ref a, ref b)| {
+          includes.push_str("  - ");
+          includes.push_str(&a);
+          includes.push_str("\n");
+        });
 
         let mut yaml_output = String::new();
 
