6e95e28d8b7801d50c9702cf485732623ba16a1b
technetos <mrgould93@gmail.com>
2018-01-09 23:31:53 -0500

    created RepoManager trait

--- hatch/src/cli/commands/ops.rs
+++ hatch/src/cli/commands/ops.rs
@@ -1,6 +1,15 @@
 use HatchResult;
 use project::Project;
+use clap::ArgMatches;
 
 pub trait ProjectOps {
   fn execute(&self, path: String, project_names: Vec<String>) -> Vec<HatchResult<Project>>;
+}
+
+pub trait RepoManager<'ops> {
+  fn fetch_dependencies(&self, repo_spec: &Vec<(String, String)>, path: &str);
+  fn get_includes(&self, args: &ArgMatches<'ops>) -> Vec<(String, String)>;
+  fn modules_path(&self, base: &str) -> String {
+    base.to_owned() + "/hatch_modules"
+  }
 }
