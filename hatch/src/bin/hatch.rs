716b701c031d62faf742fa7d8821073653994374
technetos <mrgould93@gmail.com>
2018-01-15 12:19:21 -0500

    update and build are still broken, changed how we read the current subcommand slightly to account for the fact that the error type returned now is a trait not an instance

--- hatch/src/bin/hatch.rs
+++ hatch/src/bin/hatch.rs
@@ -5,8 +5,8 @@ use std::collections::HashMap;
 use hatch::cli::Cli;
 use hatch::cli::commands::Command;
 use hatch::cli::commands::new::New;
-use hatch::cli::commands::update::Update;
-use hatch::cli::commands::build::Build;
+//use hatch::cli::commands::update::Update;
+//use hatch::cli::commands::build::Build;
 
 use hatch::hatch_error::{ HatchError, NullError };
 
@@ -17,30 +17,22 @@ fn main() {
   let new_command = Box::new(New::new());
   subcommands.insert(new_command.subcommand_name(), new_command);
 
-  let update_command = Box::new(Update::new());
-  subcommands.insert(update_command.subcommand_name(), update_command);
-
-  let build_command = Box::new(Build::new());
-  subcommands.insert(build_command.subcommand_name(), build_command);
+//  let update_command = Box::new(Update::new());
+//  subcommands.insert(update_command.subcommand_name(), update_command);
+//
+//  let build_command = Box::new(Build::new());
+//  subcommands.insert(build_command.subcommand_name(), build_command);
 
   // initialize cli with the set of subcommand
   let cli = Cli::new(subcommands.values().map(|v| v.cli_subcommand()).collect::<Vec<_>>());
 
   // execute selected subcommand
-  let result = match cli.subcommand() {
+  match cli.subcommand() {
     (subcommand_name, Some(cli_args)) => {
-      match subcommands.get(subcommand_name) {
-        Some(subcommand) => subcommand.execute(cli_args),
-        _ => vec![Err(HatchError::Null(NullError))]
+      if let Some(cmd) = subcommands.get(subcommand_name) {
+        cmd.execute(cli_args);
       }
     },
-    _ => vec![Err(HatchError::Null(NullError))]
-  };
-
-  result.iter().for_each(|r| {
-    match *r {
-      Ok(ref p) => println!("{:?}", p),
-      Err(ref e) => println!("{}", e),
-    }
-  });
+    _ => ()
+  }
 }
