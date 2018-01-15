06f935be69b3b87e0f28d436756c7b19ccc1ad5c
Danny Peck <danieljpeck93@gmail.com>
2018-01-14 23:23:32 -0800

    added the Tupfile asset

--- hatch/src/assets/builder.rs
+++ hatch/src/assets/builder.rs
@@ -1,7 +1,8 @@
 use assets::{ TupKind, PlatformKind, Arch, ProjectAsset };
 use assets::config::Config;
 use assets::tuprules::Tuprules;
-use assets::test::Tupfile;
+use assets::test::Tupfile as TestTupfile;
+use assets::tupfile::Tupfile;
 use assets::platform::{ Linux, Darwin, Windows};
 use project::{ Project, ProjectKind };
 
@@ -15,6 +16,7 @@ impl Builder {
     builder.project(&TupKind::Config, project);
     builder.project(&TupKind::TestTupfile, project);
     builder.project(&TupKind::Tuprules, project);
+    builder.project(&TupKind::Tupfile, project);
     builder.platform(&PlatformKind::Linux);
     builder.platform(&PlatformKind::Darwin);
     builder.platform(&PlatformKind::Windows);
@@ -31,7 +33,7 @@ impl Builder {
       TupKind::Config => Self::config(project),
       TupKind::TestTupfile => Self::test_tupfile(project),
       TupKind::Tuprules => Self::tuprules(project),
-      _ => ProjectAsset::new(String::new(), String::new())
+      TupKind::Tupfile => Self::tupfile(project),
     };
 
     self.assets.push(asset);
@@ -48,25 +50,25 @@ impl Builder {
   }
 
   fn config(project: &Project) -> ProjectAsset {
-    let file_path = "C++/libs/".to_owned() + project.name() + "/config.tup";
+    let file_path = String::from("./config.tup");
     let file_contents = Config::new(project.name(), project.kind()).to_string();
 
     ProjectAsset::new(file_path, file_contents)
   }
 
   fn test_tupfile(project: &Project) -> ProjectAsset {
-    let file_path = "C++/libs/".to_owned() + project.name() + "/test/Tupfile";
-    let file_contents = Tupfile::new().to_string();
+    let file_path = String::from("./test/Tupfile");
+    let file_contents = TestTupfile::new().to_string();
 
     ProjectAsset::new(file_path, file_contents)
   }
 
   fn tuprules(project: &Project) -> ProjectAsset {
-    let file_path = "C++/libs/".to_owned() + "Tuprules.tup";
+    let file_path = String::from("./Tuprules.tup");
     let project_kind = project.kind();
     let file_contents = match *project_kind {
       ProjectKind::Library(ref lib_type) =>  {
-        Tuprules::new("g++".to_owned(), false, Arch::X64, "c++1z".to_owned(), lib_type).to_string()
+        Tuprules::new(String::from("g++"), false, Arch::X64, String::from("c++1z"), lib_type).to_string()
       },
       _ => String::new()
     };
@@ -74,22 +76,29 @@ impl Builder {
     ProjectAsset::new(file_path, file_contents)
   }
 
+  fn tupfile(project: &Project) -> ProjectAsset {
+    let file_path = String::from("./Tupfile");
+    let file_contents = Tupfile::new().to_string();
+
+    ProjectAsset::new(file_path, file_contents)
+  }
+
   fn linux() -> ProjectAsset {
-    let file_path = "C++/libs/linux.tup".to_owned();
+    let file_path = String::from("./linux.tup");
     let file_contents = Linux::new().to_string();
 
     ProjectAsset::new(file_path, file_contents)
   }
 
   fn darwin() -> ProjectAsset {
-    let file_path = "C++/libs/macosx.tup".to_owned();
+    let file_path = String::from("./macosx.tup");
     let file_contents = Darwin::new().to_string();
 
     ProjectAsset::new(file_path, file_contents)
   }
 
   fn windows() -> ProjectAsset {
-    let file_path = "C++/libs/win32.tup".to_owned();
+    let file_path = String::from("./win32.tup");
     let file_contents = Windows::new().to_string();
 
     ProjectAsset::new(file_path, file_contents)
--- hatch/src/assets/tupfile.rs
+++ hatch/src/assets/tupfile.rs
@@ -1,3 +1,36 @@
+pub struct Tupfile;
+
+impl Tupfile {
+  pub fn new() -> Tupfile {
+    Tupfile
+  }
+}
+
+impl ToString for Tupfile {
+  fn to_string(&self) -> String {
+    String::from(
+"include config.tup
+include_rules
+
+# override build variables
+# VARIABLE = new_value
+
+# define custom build variables
+
+# Compile Source
+: foreach $(SOURCE_FILES) |> !compile |> $(SOURCE_OUT)/%B.o
+
+# Archive Source
+: $(SOURCE_OBJ_FILES) |> !archive |> $(SOURCE_OUT)/$(PROJECT_LIB) ../<$(PROJECT)>
+
+# Compile Tests
+: foreach $(TEST_FILES) |> !compile |> $(TEST_OUT)/%B.o
+
+# Create Link Executable
+: $(TEST_OBJ_FILES) $(SOURCE_OUT)/$(PROJECT_LIB) |> !link |> $(TEST_OUT)/$(PROJECT).test")
+  }
+}
+
 //     pub fn tupfile(project: &Project) -> ProjectAsset {
 //     let file_path = "C++/libs/".to_owned() + project.name() + "/Tupfile";
 //     let file_contents = // {{{
