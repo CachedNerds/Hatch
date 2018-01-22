ee5826a91ce3dadcdcd4146735da0b8ecee18b24
Danny Peck <danieljpeck93@gmail.com>
2018-01-22 01:59:37 -0800

    moved scripts back into .travis.yml

--- .travis_ci_helper.sh
+++ .travis_ci_helper.sh
@@ -5,6 +5,9 @@ PROJECT_NAME="hatch";
 
 cd $PROJECT_NAME;
 
+
+pip install 'travis-cargo<0.2' --user && export PATH=$HOME/.local/bin:$PATH
+
 travis-cargo build
 travis-cargo test
 travis-cargo bench
