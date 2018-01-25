d117bdfa8d64592ee5f35222a2fe1ff27f0cf657
technetos <mrgould93@gmail.com>
2018-01-24 21:34:43 -0500

    cleaned up platform_type and some used statements

--- hatch/src/task.rs
+++ hatch/src/task.rs
@@ -1,13 +1,13 @@
 use project::Project;
 use assets::builder::Builder as AssetBuilder;
+use std::path::Path;
+use hatch_error::HatchResult;
 use assets::generator;
 use assets::PlatformKind;
-use yaml;
-use hatch_error::HatchResult;
-use std::path::{ PathBuf, Path };
-use os_info;
-use os_info::Type;
 use repo::hatchfile_path;
+use yaml;
+use os_info;
+use os_info::Type::{ Macos, Windows };
 
 pub fn read_project(path: &Path) -> HatchResult<Project> {
   yaml::parse(hatchfile_path(path).as_path())
@@ -18,10 +18,9 @@ pub fn generate_assets(project: &Project) -> HatchResult<()> {
 }
 
 pub fn platform_type() -> PlatformKind {
-  let info = os_info::get();
-    match *info.os_type() {
-      Type::Macos => PlatformKind::MacOS,
-      Type::Windows => PlatformKind::Windows,
-      _ => PlatformKind::Linux
-    }
+  match *os_info::get().os_type() {
+    Macos => PlatformKind::MacOS,
+    Windows => PlatformKind::Windows,
+    _ => PlatformKind::Linux
+  }
 }
