7906c96314d62138e40063737a5016400229a66a
technetos <mrgould93@gmail.com>
2018-01-26 02:58:07 -0500

    changed ../hatch to hatch

--- .ci_helpers/appveyor_helper.bat
+++ .ci_helpers/appveyor_helper.bat
@@ -1,4 +1,4 @@
-cd ../"hatch"
+cd "hatch"
 
 cargo build --verbose --all
 cargo test --verbose --all
--- .ci_helpers/travis_ci_helper.sh
+++ .ci_helpers/travis_ci_helper.sh
@@ -3,7 +3,7 @@ set -e
 
 PROJECT_NAME="hatch";
 
-cd ../$PROJECT_NAME;
+cd $PROJECT_NAME;
 
 cargo build --verbose --all
 cargo test --verbose --all
