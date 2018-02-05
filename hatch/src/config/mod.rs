f9ad77693a8a0e50be159df429e918a52d3ac843
technetos <mrgould93@gmail.com>
2018-02-05 13:10:17 -0500

    surfaced file_config and used std::path::Path instead of String

--- hatch/src/config/mod.rs
+++ hatch/src/config/mod.rs
@@ -1,11 +1,13 @@
 pub mod env_config;
+pub mod file_config;
 
 use std::collections::HashMap;
+use std::path::Path;
 
 pub trait ConfigLoader {
   fn getConfig(&self, source: String) -> HashMap<String, String>;
 }
 
 pub trait ConfigFileLoader : ConfigLoader {
-  fn getConfigImpl(&self, path: String, file: String) -> HashMap<String, String>;
+  fn getConfigImpl(&self, path: Path, file: String) -> HashMap<String, String>;
 }
