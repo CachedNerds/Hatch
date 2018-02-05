4258705b525d0d01eac6f3d82ee80daadc880ade
technetos <mrgould93@gmail.com>
2018-02-05 14:36:49 -0500

    Changed usage of Path to &Path because the size of a reference is known at compile time and changed usage of heap alloacated String to a &str

--- hatch/src/config/mod.rs
+++ hatch/src/config/mod.rs
@@ -6,9 +6,9 @@ use std::collections::HashMap;
 use std::path::Path;
 
 pub trait ConfigLoader {
-  fn getConfig(&self, source: String) -> HashMap<String, String>;
+  fn getConfig(&self, source: &str) -> HashMap<String, String>;
 }
 
 pub trait ConfigFileLoader : ConfigLoader {
-  fn getConfigImpl(&self, path: Path, file: String) -> HashMap<String, String>;
+  fn getConfigImpl(&self, path: &Path, file: &str) -> HashMap<String, String>;
 }
