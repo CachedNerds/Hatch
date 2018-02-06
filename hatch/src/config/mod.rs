080587af964c95f2e5e917e612ae739774e57f62
technetos <mrgould93@gmail.com>
2018-02-06 14:13:24 -0500

    changed file_config to yaml_config and delegated parsing from the loader to somewhere else via a visitor

--- hatch/src/config/mod.rs
+++ hatch/src/config/mod.rs
@@ -1,14 +1,9 @@
-pub mod env_config;
-pub mod file_config;
-pub mod cli_config;
+pub mod yaml_config;
 
-use std::collections::HashMap;
 use std::path::Path;
+use hatch_error::HatchResult;
+use visitor::config_visitor::YamlConfigVisitor;
 
 pub trait ConfigLoader {
-  fn getConfig(&self, source: &str) -> HashMap<String, String>;
-}
-
-pub trait ConfigFileLoader : ConfigLoader {
-  fn getConfigImpl(&self, path: &Path, file: &str) -> HashMap<String, String>;
+  fn accept(&self, visitor: Box<&YamlConfigVisitor>);
 }
