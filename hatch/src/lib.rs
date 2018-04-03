db925e3919fae764d07b56dfa192e72efc08f759
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2018-04-03 00:53:47 -0700

    Do some deriving

--- hatch/src/lib.rs
+++ hatch/src/lib.rs
@@ -1,5 +1,7 @@
 #![forbid(unsafe_code)]
-
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+extern crate serde_yaml;
 
 #[macro_use]
 pub extern crate clap;
@@ -9,6 +11,8 @@ pub extern crate git2;
 pub extern crate failure;
 pub extern crate os_info;
 pub extern crate reqwest;
+
+
 
 pub mod project;
 pub mod cli;
@@ -24,5 +28,38 @@ pub mod locations;
 mod tests {
   #[test]
   fn it_works() {
+    use serde_yaml;
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    struct Length { a: f64 }
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    struct Point { x: f64, y: Length }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    struct Cfg {
+      compiler: String,
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    struct CompiledProj {
+      name: String,
+      version: String,
+      cfg: Cfg,
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    struct HeaderOnlyProj {
+      name: String,
+      version: String,
+      cfg: Cfg,
+    }
+    // let proj = Proj { name: "foo".to_string(), version: "0.1.0".to_string(), cfg: Some(Cfg { compiler: "g++".to_string() }) };
+    let proj = Proj { name: "foo".to_string(), version: "0.1.0".to_string(), cfg: None };
+    let s = serde_yaml::to_string(&proj).unwrap();
+    println!("{}", s);
+//    assert_eq!(s, "---\nx: 1\n\"y\": 2");
+//
+//    let deserialized_point: Point = serde_yaml::from_str(&s).unwrap();
+//    assert_eq!(point, deserialized_point);
   }
 }
