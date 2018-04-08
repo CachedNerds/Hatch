e0c8208c0694199c8fc1129299e3be4f5172fcd3
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2018-04-08 13:54:24 -0700

    cleanup project mod

--- hatch/src/project/mod.rs
+++ hatch/src/project/mod.rs
@@ -1,58 +1,6 @@
-//pub mod build;
-//
-//use std::fmt;
-//use std::path::PathBuf;
-//use self::build::Config;
-//use deps::dependency::Dependency;
-//
-//#[derive(Debug)]
-//pub struct Project {
-//  name: String,
-//  version: String,
-//  config: Config,
-//  deps: Vec<Dependency>,
-//  path: PathBuf
-//}
-//
-//impl Project {
-//  pub fn new(name: String,
-//             version: String,
-//             config: Config,
-//             deps: Vec<Dependency>,
-//             path: PathBuf) -> Project
-//  {
-//    Project {
-//      name,
-//      version,
-//      config,
-//      deps,
-//      path
-//    }
-//  }
-//
-//  pub fn name(&self) -> &str {
-//    self.name.as_ref()
-//  }
-//
-//  pub fn version(&self) -> &str {
-//    self.version.as_ref()
-//  }
-//
-//  pub fn config(&self) -> &Config {
-//    self.config.as_ref()
-//  }
-//
-//  pub fn deps(&self) -> &Vec<Dependency> {
-//    self.deps.as_ref()
-//  }
-//
-//  pub fn path(&self) -> &PathBuf {
-//    &self.path
-//  }
-//}
-// ////#[derive(Debug)]
-////pub enum LibraryKind { Shared, Static }
-//
+use platform::arch::Arch;
+use deps::dependency::Dependency;
+use project::ProjectKind;
 
 use cli::commands::{ BIN, STATIC, SHARED, HEADER };
 
@@ -69,24 +17,71 @@ impl  ProjectKind {
       _ => ProjectKind::Static
     }
   }
-
   pub fn default() -> ProjectKind {
     ProjectKind::Static
   }
 }
 
-//impl AsRef<ProjectKind> for ProjectKind {
-//  fn as_ref(&self) -> &ProjectKind {
-//    &self
-//  }
-//}
-//
-////impl fmt::Display for ProjectKind {
-////  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-////    match *self {
-////      ProjectKind::Library(LibraryKind::Shared) => write!(f, "shared-lib"),
-////      ProjectKind::Library(LibraryKind::Static) => write!(f, "static-lib"),
-////      ProjectKind::Binary => write!(f, "binary"),
-////    }
-////  }
-////}
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum Target { Debug, Release }
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct CompilerOptions {
+  pub kind: ProjectKind,
+  pub compiler: String,
+  pub compiler_flags: String,
+  pub linker_flags: String,
+  pub arch: Arch,
+  pub target: Target,
+}
+
+impl CompilerOptions {
+  pub fn new(kind: ProjectKind, compiler: String, compiler_flags: String, linker_flags: String,
+             arch: Arch, target: Target) -> CompilerOptions {
+    CompilerOptions {
+      kind,
+      compiler,
+      compiler_flags,
+      linker_flags,
+      arch,
+      target
+    }
+  }
+
+  pub fn default_for_kind(kind: ProjectKind) -> CompilerOptions {
+    let compiler: String = String::from("g++");
+    let compiler_flags= String::from("-c");
+    let linker_flags= String::from("-v");
+    let mut arch: Arch = Arch::X64;
+    if let Some(architecture) = Arch::architecture() {
+      arch = architecture;
+    }
+    let target: Target = Target::Debug;
+
+    CompilerOptions {
+      kind,
+      compiler,
+      compiler_flags,
+      linker_flags,
+      arch,
+      target
+    }
+  }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Project {
+  pub name : String,
+  pub version: String,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub config: Option<CompilerOptions>,
+  #[serde(skip_serializing_if = "Vec::is_empty")]
+  pub includes: Vec<Dependency>,
+}
+
+impl Project {
+  pub fn new(name: String, version: String, config: Option<CompilerOptions>,
+             includes: Vec<Dependency>) -> Project {
+    Project { name, version, config, includes }
+  }
+}
