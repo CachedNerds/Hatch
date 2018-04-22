9b9506ffa48d2d8f4396272d55a75e396a44d73e
Danny Peck <danieljpeck93@gmail.com>
2018-04-22 00:07:27 -0700

    Revert "ran rustfmt on branch"
    This reverts commit fc2c373a9e3cabee4f9540210248668f639a35cf.

--- hatch/src/assets/janitor.rs
+++ hatch/src/assets/janitor.rs
@@ -1,36 +1,30 @@
-use assets::{Asset, ProjectAsset};
-use hatch_error::{HatchResult, ResultExt};
+use assets::{ Asset, ProjectAsset };
+use hatch_error::{ HatchResult, ResultExt };
 use std::path::PathBuf;
 use std::fs;
 
 fn remove_file(file_path: &PathBuf) -> HatchResult<()> {
-    lsfs::remove_file(&file_path)?;
+  fs::remove_file(&file_path)?;
 
-    Ok(())
+  Ok(())
 }
 
 fn remove_dir_all(dir_path: &PathBuf) -> HatchResult<()> {
-    fs::remove_dir_all(&dir_path)?;
+  fs::remove_dir_all(&dir_path)?;
 
-    Ok(())
+  Ok(())
 }
 
 pub fn remove_asset(asset: &ProjectAsset) -> HatchResult<()> {
-    let asset_path = asset.path().join(asset.name());
-    remove_file(&asset_path)
+  let asset_path = asset.path().join(asset.name());
+  remove_file(&asset_path)
 }
 
 pub fn remove_assets(assets: &Vec<ProjectAsset>) -> Vec<HatchResult<()>> {
-    assets
-        .iter()
-        .map(|asset| remove_asset(asset))
-        .collect::<_>()
+  assets.iter().map(|asset| remove_asset(asset)).collect::<_>()
 }
 
 pub fn remove_targets(project_path: &PathBuf) -> Vec<HatchResult<()>> {
-    let targets = vec!["target", "test/target"];
-    targets
-        .iter()
-        .map(|target| remove_dir_all(&project_path.join(target)))
-        .collect::<_>()
+  let targets = vec!["target", "test/target"];
+  targets.iter().map(|target| remove_dir_all(&project_path.join(target))).collect::<_>()
 }
