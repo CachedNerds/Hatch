ecfe3d67db5dd86170ec56d064bfab8ce382392b
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2018-05-06 18:47:24 -0700

    fix fixtures

--- hatch/src/assets/tests/fixtures.rs
+++ hatch/src/assets/tests/fixtures.rs
@@ -1,14 +1,13 @@
 use project::{Project, ProjectKind};
 use platform::arch::Arch;
-use std::path::PathBuf;
 use project::CompilerOptions;
 use project::Target;
 
 pub fn project(kind: ProjectKind) -> Project {
-    let config = CompilerOptions::new(
+    let compiler_options = CompilerOptions::new(
         String::from("g++"),
-        vec![String::from("-c"), String::from("--std=c++1z")],
-        vec![String::from("-v")],
+        vec![String::from("-c"), String::from("--std=c++1z")].join(' '.to_string().as_str()),
+        vec![String::from("-v")].join(' '.to_string().as_str()),
         Arch::X64,
         Target::Release,
     );
@@ -16,14 +15,19 @@ pub fn project(kind: ProjectKind) -> Project {
     let project = Project::new(
         String::from("test"),
         String::from("0.1.0"),
-        config,
+        ProjectKind::Static,
+        Some(compiler_options),
         Vec::new(),
-        PathBuf::from("./"),
     );
 
     project
 }
 
 pub fn project_with_compiler_options(kind: ProjectKind, compiler_options: CompilerOptions) -> Project {
-    Project::new(String::from("test"), String::from("0.1.0"), kind, compiler_options, PathBuf::from("./"))
+    Project::new(
+        String::from("test"),
+        String::from("0.1.0"),
+        kind,
+        Some(compiler_options),
+        Vec::new())
 }
