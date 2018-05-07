75c99512f39a768115b7c542675696e93a87275a
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2018-05-06 18:42:12 -0700

    add another fixture to help me out

--- hatch/src/assets/tests/fixtures.rs
+++ hatch/src/assets/tests/fixtures.rs
@@ -1,11 +1,11 @@
 use project::{Project, ProjectKind};
-use project::build::{Config, Target};
 use platform::arch::Arch;
 use std::path::PathBuf;
+use project::CompilerOptions;
+use project::Target;
 
 pub fn project(kind: ProjectKind) -> Project {
-    let config = Config::new(
-        kind,
+    let config = CompilerOptions::new(
         String::from("g++"),
         vec![String::from("-c"), String::from("--std=c++1z")],
         vec![String::from("-v")],
@@ -22,4 +22,8 @@ pub fn project(kind: ProjectKind) -> Project {
     );
 
     project
+}
+
+pub fn project_with_compiler_options(kind: ProjectKind, compiler_options: CompilerOptions) -> Project {
+    Project::new(String::from("test"), String::from("0.1.0"), kind, compiler_options, PathBuf::from("./"))
 }
