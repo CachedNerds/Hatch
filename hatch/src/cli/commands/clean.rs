5794a18919e725a6daaa838889c39fd17243d7e7
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2018-06-28 23:30:29 -0700

    fix Command trait drift fn impls in clean

--- hatch/src/cli/commands/clean.rs
+++ hatch/src/cli/commands/clean.rs
@@ -60,21 +60,9 @@ impl<'update> Clean {
 }
 
 impl<'command> Command<'command> for Clean {
-  fn cli_subcommand(&self) -> App<'command, 'command> {
-    SubCommand::with_name(&self.name)
-      .about("Removes volatile project artifacts.")
-      .version("0.1.0")
-      .author("Danny Peck <danieljpeck93@gmail.com>")
-  }
-
-  fn subcommand_name(&self) -> &'static str {
-    self.name
-  }
-
   fn execute(&self, args: &ArgMatches<'command>) -> HatchResult<()> {
     let project = task::read_project(&self.project_path(args))?;
     self.clean(&project)?;
-
     Ok(())
   }
 }
